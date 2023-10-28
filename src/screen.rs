4599bfd8281b2f94082f85daa8a6c2bf51b67f07
bay9990909 <75759591+bay9990909@users.noreply.github.com>
2023-10-28 11:19:24 +0200

    Update screen.rs
    peppe commit editing#1

--- src/screen.rs
+++ src/screen.rs
@@ -3,7 +3,7 @@ use screenshots::{self, Screen, display_info};
 use image::{RgbaImage, Rgb, ImageBuffer};
 use std::error::Error;
 use std::time::{Instant};
-
+use eframe::egui::*;
 #[derive(Debug)]
 struct ErrorScreenshot(String);
 
@@ -35,3 +35,46 @@ pub fn screenshot () -> Result < RgbaImage, Box<dyn Error>> {
         return Err("Nessuno Schermo disponibile")?;
     }
 }
+
+
+    pub fn ui_content( ui: &mut Ui, lines: &mut Vec<Vec<Pos2>>, stroke: &mut Stroke) -> eframe::egui::Response {
+        
+        let (mut response, painter) =
+            ui.allocate_painter(ui.available_size_before_wrap(), Sense::drag());
+            
+        let to_screen = emath::RectTransform::from_to(
+            Rect::from_min_size(Pos2::ZERO, response.rect.square_proportions()),
+            response.rect,
+        );
+        let from_screen = to_screen.inverse();
+
+        if lines.is_empty() {
+            lines.push(vec![]);
+        }
+
+        let current_line = lines.last_mut().unwrap();
+
+        if let Some(pointer_pos) = response.interact_pointer_pos() {
+            let canvas_pos = from_screen * pointer_pos;
+            if current_line.last() != Some(&canvas_pos) {
+                current_line.push(canvas_pos);
+                response.mark_changed();
+            }
+        } else if !current_line.is_empty() {
+            lines.push(vec![]);
+            response.mark_changed();
+        }
+
+        let shapes = 
+            lines
+            .iter()
+            .filter(|line| line.len() >= 2)
+            .map(|line| {
+                let points: Vec<Pos2> = line.iter().map(|p| to_screen * *p).collect();
+                eframe::egui::Shape::line(points, *stroke)
+            });
+
+        painter.extend(shapes);
+            
+        response
+    }
