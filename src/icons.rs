1aec8aae59d6265e8e871b20c2fa08443ce9cd72
bay9990909 <75759591+bay9990909@users.noreply.github.com>
2023-12-20 16:22:13 +0100

    Create icons.rs
    added icons.rs

---
+++ src/icons.rs
@@ -0,0 +1,16 @@
+use egui::{Image};
+
+
+lazy_static::lazy_static! {
+pub static ref BACK :Image<'static>  = Image::new(egui::include_image!("./icons/back.png"));
+pub static ref CIRCLE :Image<'static> = Image::new(egui::include_image!("./icons/circle.png"));
+pub static ref SCISSOR :Image<'static> = Image::new(egui::include_image!("./icons/scissor.png"));
+pub static ref CURSOR :Image<'static> = Image::new(egui::include_image!("./icons/cursor.png"));
+pub static ref ERASER :Image<'static> = Image::new(egui::include_image!("./icons/eraser.png"));
+//pub static ref HIGHLIGHT :Image<'static> = Image::new(egui::include_image!(".../icons/circle.png"));
+pub static ref SEGMENT :Image<'static> = Image::new(egui::include_image!("./icons/segment.png"));
+pub static ref FREE :Image<'static> = Image::new(egui::include_image!("./icons/free.png"));
+pub static ref RECTANGLE :Image<'static> = Image::new(egui::include_image!("./icons/rectangle.png"));
+//pub static ref REDO :Image<'static> = Image::new(egui::include_image!("../assets/icons/light/redo.png"));
+pub static ref TEXT :Image<'static> = Image::new(egui::include_image!("./icons/text.png"));
+}
