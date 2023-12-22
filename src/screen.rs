2e996320f0fec2b7d17034de34f142de23694827
SilvioChito <93883116+SilvioChito@users.noreply.github.com>
2023-12-22 14:22:52 +0100

    Update screen.rs

--- src/screen.rs
+++ src/screen.rs
@@ -1,37 +1,18 @@
 
 use screenshots::{self, Screen, display_info};
-use image::{RgbaImage, Rgb, ImageBuffer};
+use image::{RgbaImage, Rgb, ImageBuffer, imageops};
 use std::error::Error;
-use std::time::{Instant};
+use std::ptr;
+use std::thread::sleep;
+use std::time::{Instant, Duration};
 use egui::*;
+
+
+use crate::Schermata;
 #[derive(Debug)]
 struct ErrorScreenshot(String);
 
-pub fn screenshot () -> Result <RgbaImage, Box<dyn Error>> { 
-    let start = Instant::now();
-    let display_infos: Vec<display_info::DisplayInfo> = screenshots::display_info::DisplayInfo::all().unwrap();
-    let screens = Screen::all().unwrap();
-    if display_infos.len() == 1 {
-        let screen = Screen::new(&display_infos[0]);
-        println!("capturer {screen:?}");
-        let mut raw_image = screen.capture().unwrap();
-        return Ok( raw_image);
-    }
-    else{
-        let screen = Screen::new(&display_infos[0]);
-        for screen in screens {
-            println!("capturer {screen:?}");
-            let mut raw_image: image::ImageBuffer<image::Rgba<u8>, Vec<u8>> = screen.capture().unwrap();
-            return Ok( raw_image);
-        }
-        return Err("Nessuno Schermo disponibile")?;
-    }
-}
-
-
-
-
-pub fn screenshot2 (used_monitor: usize) -> Result <RgbaImage, Box<dyn Error>> { 
+pub fn screenshot (used_monitor: usize) -> Result <RgbaImage, Box<dyn Error>> { 
     let start = Instant::now();
     let display_infos: Vec<display_info::DisplayInfo> = screenshots::display_info::DisplayInfo::all().unwrap();
     let screen = Screen::new(&display_infos[used_monitor]);
@@ -39,6 +20,50 @@ pub fn screenshot2 (used_monitor: usize) -> Result <RgbaImage, Box<dyn Error>> {
     let mut raw_image = screen.capture().unwrap();
     return Ok( raw_image);
 }
+
+
+pub fn ui_content( ui: &mut Ui, lines: &mut Vec<Vec<Pos2>>, stroke: &mut Stroke, resp: Response, dim: Vec2) -> egui::Response {
+
+    let (mut response, painter) =
+        //ui.allocate_painter(dim , Sense::drag());
+        ui.allocate_painter(ui.available_size_before_wrap() , Sense::click_and_drag());
+        
+    let to_screen = emath::RectTransform::from_to(
+        Rect::from_min_size(response.rect.min, response.rect.square_proportions()),
+        response.rect,
+    );
+    let from_screen = to_screen.inverse();
+
+    if lines.is_empty() {
+        lines.push(vec![]);
+    }
+
+    let current_line = lines.last_mut().unwrap();
+
+    if let Some(pointer_pos) = response.interact_pointer_pos() {
+        let canvas_pos = from_screen * pointer_pos;
+        if current_line.last() != Some(&canvas_pos) {
+            current_line.push(canvas_pos);
+            response.mark_changed();
+        }
+    } else if !current_line.is_empty() {
+        lines.push(vec![]);
+        response.mark_changed();
+    }
+
+    let shapes = 
+        lines
+        .iter()
+        .filter(|line| line.len() >= 2)
+        .map(|line| {
+            let points: Vec<Pos2> = line.iter().map(|p| to_screen * *p).collect();
+            egui::Shape::line(points, *stroke)
+        });
+
+    painter.extend(shapes);
+        
+    response
+    }
 
     pub fn make_screenshot(ctx: &Context, image: &mut RgbaImage, texture : &mut Option<TextureHandle>, schermata: &mut Schermata, used_monitor: usize){
         if used_monitor == 9999{
@@ -86,4 +111,4 @@ pub fn screenshot2 (used_monitor: usize) -> Result <RgbaImage, Box<dyn Error>> {
     }
 
     Ok(combined_image)
-    }
+}
